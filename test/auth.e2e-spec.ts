a6b167578803a630e63fa603b74c30a7e41e33f5
Georgi Mateev <georgi.mateev@mentormate.com>
2024-05-30 18:58:10 +0300

    test: signup test with cookie setting

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,22 @@ describe('Authentication system', () => {
         expect(email).toBe(email)
       })
   });
+
+  it('signup as new user then get currently logged user', async () => {
+    const email = 'email@email.com'
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201)
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200)
+
+    expect(body.email).toEqual(email)
+  })
 });
